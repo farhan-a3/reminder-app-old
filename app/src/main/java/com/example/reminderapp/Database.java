a8d81fa07e4b5feb881ea1d96ba8364bbf9a245f
Culty48 <robcultraro@gmail.com>
2022-11-05 12:42:04 -0400

    database start

---
+++ app/src/main/java/com/example/reminderapp/Database.java
@@ -0,0 +1,35 @@
+package com.example.reminderapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final int VERSION = 2;
+    private static final String NAME = "reminder_List_Database";
+    private static final String REMINDER_TABLE = "reminder";
+    private static final String ID = "id";
+    private static final String REMINDER_NAME = "name";
+    private static final String CREATE_REMINDER_TABLE = "CREATE TABLE " + REMINDER_TABLE + "(" +
+            ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + REMINDER_NAME + "TEXT)";
+
+    private SQLiteDatabase db;
+
+    public Database(Context context) {
+        super(context, NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_REMINDER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + REMINDER_TABLE);
+        onCreate(db);
+    }
+
+
+}
